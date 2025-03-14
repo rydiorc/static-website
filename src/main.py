79c38d0917f4a0e17c46c3aac1fdd0c39f520ae7
rydiorc <rydin.work@gmail.com>
2025-03-14 09:18:13 -0300

    #8: adds copy from static to public

--- src/main.py
+++ src/main.py
@@ -1,20 +1,35 @@
 from textnode import *
 from htmlnode import *
 from func import *
+import os
+import shutil
 
 def main():
-    md = """
-# heading 1
+    static = "static"
+    public = "public"    
+    if os.path.exists(static):
+        copy_dir(static, public, True)
+        
+def copy_dir(dir, destination, is_top_level=False):
+    if is_top_level and os.path.exists(destination):
+        print(f"removing {destination}")
+        shutil.rmtree(destination)
+        print(f"recreating {destination}")
+        os.mkdir(destination)
+    elif not os.path.exists(destination):
+        print(f"creating {destination}")
+        os.mkdir(destination)
 
-### heading 3
-
-> quote
-> is something
-> like this
-    """
-
-    node = markdown_to_html_node(md)    
-    html = node.to_html()
-    print(html)
+    if os.path.exists(dir):
+        files = os.listdir(dir)
+        for f in files:
+            src = os.path.join(dir, f)
+            dst = os.path.join(destination, f)
+            if os.path.isfile(src):
+                # Copy file
+                print(f"Copying {src} to {dst}")
+                shutil.copy(src, dst)
+            if os.path.isdir(src):
+                copy_dir(src, dst)
 
 main()
